0c87d5c30861d8ed86fff845d2ee3673f327afb1
Agnieszka Korczak <korczak.a.m@gmail.com>
2018-12-31 01:04:46 +0100

    Add project content

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use('/static', express.static('assets'));
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/auth/google', function(req, res){
+    res.render('logged');
+});
+
+var server = app.listen(3000);
+
+app.use(function(req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
+});
+
